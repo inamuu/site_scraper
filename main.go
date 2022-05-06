73527dac63aef4ec8b3c0e7483cc5a61f4c69c44
inamuu <8310973+inamuu@users.noreply.github.com>
2022-05-06 18:24:04 +0900

    Implement get html source from example site

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"github.com/joho/godotenv"
 )
 
 type payload struct {
@@ -29,11 +32,46 @@ func slackNotify(webhookUrl string) (err error) {
 	return
 }
 
-func main() {
-	webhookUrl := os.Getenv("SLACKURL")
-	err := slackNotify(webhookUrl)
+func requestUrl(url string) (err error) {
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
-	fmt.Println("ok")
+
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+
+	return
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading env file")
+	}
+
+	url := os.Getenv("REQUESTURL")
+	err = requestUrl(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//webhookUrl := os.Getenv("SLACKURL")
+	//err := slackNotify(webhookUrl)
+	//if err != nil {
+	//	fmt.Println("ng")
+	//	return
+	//}
+	//fmt.Println("ok")
 }
